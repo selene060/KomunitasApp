3a5e33c738c7aa32f9e2085eb0b7cd265af329a9
selene060 <sitizahwa060@gmail.com>
2025-03-17 14:21:18 +0700

    FE landing page

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+      <MainTheHeader />
+      <main class="pt-32 pb-20">
+        <MainHeroSection />
+        <MainFeatureSection />
+      </main>
+      <MainTheFooter />
+    </div>
+  </template>
+  
+  <script setup>
+  
+  </script>
+  
+  
